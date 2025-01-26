29b7ab61b24d4614bf2c186fa1502b8984cfbb19
YatsuZ <yassine.zaoui28062003@gmail.com>
2025-01-26 23:20:13 +0100

    ajoue de message

--- code/main.cpp
+++ code/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yzaoui <yzaoui@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 17:27:32 by yzaoui            #+#    #+#             */
-/*   Updated: 2025/01/26 20:09:11 by yzaoui           ###   ########.fr       */
+/*   Updated: 2025/01/26 23:19:06 by yzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,27 @@
 
 void TO_DO(void)
 {
-	std::cout << RED << "Netoyer re adpater les fichier et ameliorer le code " << NOCOLOR << std::endl;
+	std::cout << BLUE << "SALUT voici ce qu'il a faire." << NOCOLOR << std::endl;
+	std::cout <<"Faire linterpretation des action recu du client irssi pour cela."  << std::endl;
+	std::cout <<"aller dans le fichier" << YELLOW << "./class/Cmd_irssi/Cmd_irssi.cpp" << NOCOLOR << " dans cette methode : " << MAGENTA << "Cmd_irssi::init_action(void)" << NOCOLOR << std::endl;
+	std::cout << GREEN << "Pour interpreter la premiere commande" << NOCOLOR << std::endl;
+	std::cout << "Il faudra aussi aller dans " << YELLOW << "./header/Constante.hpp" << NOCOLOR << " pour ajouter dans lenum " << RED << "ACTION" << NOCOLOR
+	<< " le nouveaux ACTION enum il faudra aussi aller dans " << YELLOW << "./header/Constante.hpp" << NOCOLOR << std::endl;
+	std::cout << "Et" << std::endl;
+	std::cout << YELLOW << "./utils/define_array_and_get_element_of_array.cpp" << NOCOLOR << " pour ajouter le message de cette nouvelle action ce quelle fais ou etc." << std::endl;
+	std::cout << RED << "/!\\ RESPECTER LES COMMENTAIRES LE MEME ORDRE TRES IMPORTANT. " << NOCOLOR << std::endl;
+	
+	std::cout << "Pour finir aller dans Irssi_serv.hpp pour crée la nouvelle methode de laction. Ne pas oublier de rajouter dans le constructeur et hope FINI." <<std::endl;
+	std::cout << std::endl;
+	std::cout << "------------------" << std::endl;
+	std::cout << "--BONNE-CHANCE--" << GREEN << ";D" << NOCOLOR << std::endl;
+
 }
 
 int	main(int argc, char **argv)
 {
+	(void)	argc;
+	(void)	argv;
 	TO_DO();
 	std::cout << "------------------" << std::endl;
 	try
