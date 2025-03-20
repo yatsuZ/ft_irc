1468efa8b1359f6297c8a7f272fdfc3e9ccf6916
Samira MLAMALI <smlamali@bess-f3r6s2.clusters.42paris.fr>
2025-03-20 21:12:15 +0100

    Debug reception/envoi des message et implementer les actions du serv

--- code/class/Irssi_serv/other/execution.cpp
+++ code/class/Irssi_serv/other/execution.cpp
@@ -6,7 +6,7 @@
 /*   By: smlamali <smlamali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 23:16:18 by yzaoui            #+#    #+#             */
-/*   Updated: 2025/03/17 15:37:22 by smlamali         ###   ########.fr       */
+/*   Updated: 2025/03/20 21:00:00 by smlamali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ Reaction_Serv	Irssi_serv::do_action(Cmd_irssi &current_cmd, UserHuman * current_
 		return (ft_cap(current_cmd, current_user, current_pollfd, index_of_current_pollfd));
 	if (current_user && !current_user->_get_is_connect() && current_cmd.get_action() != PASS ) //&& current_cmd.get_action() == CAP
 	{
-		std::cout << "AAAAAAAAAAAAAAAAAAH" << std::endl;
+		std::cout << "is user already connected ?=> " << current_user->_get_is_connect() << std::endl;
 		send_message(":" + this->get_name() + " :ERROR Closing Link localhost (Bad Password)" + CRLF, current_pollfd);
 		return (PASS_SERV);
 	}
@@ -51,36 +51,36 @@ void	Irssi_serv::exec(void)
 	std::cout << YELLOW << "Execution du Serveur ..." << NOCOLOR << std::endl;
 
 	// Boucle principale
-	while (true)
-	{
-		// Poll pour attendre un événement
-		int ret = poll(this->_all_pollfd.data(), this->_all_pollfd.size(), 5); // Attente pour des événements +  Utilise _all_pollfd.data() pour obtenir un pointeur sur le tableau interne
-		if (ret < 0)
-			this->_throw_except("Erreur de la fonction poll()");
-		if (this->_all_pollfd[0].revents & POLLIN)
-			this->connect();// Nouvelle connexion
-		for (size_t i = 1; i < this->_all_pollfd.size(); i++)
-		{
-			current_pollfd = this->_all_pollfd[i];
+	while (!loopChecker(false))
+    {
+        // Poll pour attendre un événement
+        int ret = poll(this->_all_pollfd.data(), this->_all_pollfd.size(), 5); // Attente pour des événements +  Utilise _all_pollfd.data() pour obtenir un pointeur sur le tableau interne
+        if (ret < 0 && !loopChecker(false))
+            this->_throw_except("Erreur de la fonction poll()");
+        if (this->_all_pollfd[0].revents & POLLIN)
+            this->connect();// Nouvelle connexion
+        for (size_t i = 1; i < this->_all_pollfd.size(); i++)
+        {
+            current_pollfd = this->_all_pollfd[i];
 
-			if (current_pollfd.revents & POLLIN)
-			{
-				list_cmd = this->link(current_pollfd);
+            if (current_pollfd.revents & POLLIN)
+            {
+                list_cmd = this->link(current_pollfd);
 
-				for (size_t index_cmd = 0; index_cmd < list_cmd.size(); ++index_cmd)
-				{
-					Cmd_irssi iter_cmd_irssi(list_cmd[index_cmd]);
-					Reaction_Serv reaction = do_action(iter_cmd_irssi, _get_userhuman_by_index_of_pollfd(i), current_pollfd, i);
-					if (reaction == PASS_SERV)
-						break;
-					else if (reaction == STOP)
-						return;
-				}
-				list_cmd.clear();
-				std::cout << std::endl;
-			}
-		}
-	}
+                for (size_t index_cmd = 0; index_cmd < list_cmd.size(); ++index_cmd)
+                {
+                    Cmd_irssi iter_cmd_irssi(list_cmd[index_cmd]);
+                    Reaction_Serv reaction = do_action(iter_cmd_irssi, _get_userhuman_by_index_of_pollfd(i), current_pollfd, i);
+                    if (reaction == PASS_SERV)
+                        break;
+                    else if (reaction == STOP)
+                        return;
+                }
+                list_cmd.clear();
+                std::cout << std::endl;
+            }
+        }
+    }
 }
 
 /*
